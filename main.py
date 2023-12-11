373d9b066e8543250e7cce3da4ec7b84ad8a34a4
Shivam Jha <sjha42706@gmail.com>
2023-12-11 17:10:26 +0530

    I basically did shit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import win32com.client
 import speech_recognition as sr
-import os
+import webbrowser
 def say(script):
     '''This fucntion says out loud whatever string is given to it'''
     speaker = win32com.client.Dispatch('SAPI.SpVoice')
@@ -10,21 +10,26 @@ def take_command():
     '''This function takes voice input from the user'''
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        say('Listening')
-        print('Listening')
-        r.pause_threshold = 1
+        r.pause_threshold = 0.5
         audio = r.listen(source)
         try:
+            print('Recognizing')
             query = r.recognize_google(audio, language= 'en-in')
             print(f'User said: {query}')
             return query
         except Exception as e:
-            return 'Some Error Occured. Sorry from Jarvis'
+            return 'Some Error Occurred. Sorry from Jarvis'
 while True:
     print('Listening')
     say('Listening')
     query = take_command()
+    say('Command accepted')
     say(query)
-    if  'Open Youtube'.lower() in query.lower():
-        webbrowser.open('https://youtube.com')
-        say('Opening youtube')
+    sites = [['youtube','https://www.youtube.com'],['wikipedia','https://www.wikipedia.com'],['google','https://www.google.com'],['quora','https://www.quora.com'],['facebook','https://www.facebook.com'],['Github','https://www.github.com'],['Reddit','https://www.reddit.com'],['Discord','https://www.discord.com'],['Instagram','https://www.instagram.com'],['Netflix','https://www.Netflix.com'],['Spotify','https://www.Spotify.com']]
+    for site in sites:
+        if  f'Open {site[0]}'.lower() in query.lower():
+            say(f'Opening {site[0]}')
+            webbrowser.open(site[1])
+    if 'stop'.lower() in query.lower():
+        say('Okay Sir,Jarvis is turning off')
+        quit()
