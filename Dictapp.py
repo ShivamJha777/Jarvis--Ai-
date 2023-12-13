2dfd815fd4dbd035ffdbb5b6facb48d8735f3ccf
Shivam Jha <sjha42706@gmail.com>
2023-12-13 22:00:23 +0530

    Just added a few functions
    I gain did some shit

---
+++ Dictapp.py
@@ -0,0 +1,78 @@
+import os
+import pyautogui
+import webbrowser
+import win32com.client
+from time import sleep
+def say(script):
+    '''This function says out loud whatever string is given to it'''
+    speaker = win32com.client.Dispatch('SAPI.SpVoice')
+    return speaker.Speak(script)
+dictapp = {'commandprompt':'cmd','paint':'paint','word':'winword',"excel":'excel',"chrome":'chrome','vscode':'code','powerpoint':'powerpnt','brave':'brave','pycharm':'pycharm'}
+def openappweb(query):
+    say('Launching ,Sir')
+    if '.com' in query or 'co.in'in query or '.org' in query:
+        query = query.replace('open','')
+        query = query.replace('jarvis','')
+        query = query.replace('launch','')
+        query = query.replace(' ','')
+        webbrowser.open(f'https://www.{query}')
+    else:
+        keys = list(dictapp.keys())
+        for app in keys:
+            if app in query:
+                os.system(f'start {dictapp[app]}')
+def closeappweb(query):
+    say('Closing,Sir')
+    if 'one tab' in query or '1 tab' in query:
+        pyautogui.hotkey('ctrl','w')
+        say('All tabs closed boss')
+    elif 'two tab' in query or '2 tab' in query:
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        say('All tabs closed,Boss')
+    elif 'three tab' in query or '3 tab' in query:
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        say('All tabs closed,Boss')
+    elif 'four tab' in query or '4 tab' in query:
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        say('All tabs closed,Boss')
+    elif 'five tab' in query or '5 tab' in query:
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        say('All tabs closed,Boss')
+    elif 'six tab' in query or '5 tab' in query:
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(0.5)
+        pyautogui.hotkey('ctrl', 'w')
+        say('All tabs closed,Boss')
+    else:
+        keys = list(dictapp.keys())
+        for app in keys:
+            if app in query:
+                os.system(f'taskkill /f /in {dictapp[app]}.exe')
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import wikipedia
-import win32com.client
-import speech_recognition as sr
-import webbrowser
 import pywhatkit
-import datetime
+import webbrowser
 import requests
 from bs4 import BeautifulSoup
+import win32com.client
+import speech_recognition as sr
+import wikipedia
+import datetime
 def say(script):
-    '''This fucntion says out loud whatever string is given to it'''
+    '''This function says out loud whatever string is given to it'''
     speaker = win32com.client.Dispatch('SAPI.SpVoice')
     return speaker.Speak(script)
 def take_command():
     '''This function takes voice input from the user'''
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 0.7
-        r.energy_threshold = 400
+        r.pause_threshold = 0.5
+        r.energy_threshold = 450
         audio = r.listen(source)
         try:
             say('Recognizing')
@@ -25,16 +25,18 @@ def take_command():
         except Exception as e:
             return 'Some Error Occurred. Sorry from Jarvis'
 def Greet_Me():
+    '''This function greets the user'''
     hour = int(datetime.datetime.now().hour)
     if hour>= 0 and hour <= 12:
         say('Good morning,Boss')
-    if hour > 12 and hour <= 15:
+    elif hour > 12 and hour <= 15:
         say('Good Afternoon,Boss')
-    if hour > 15 and hour <= 19:
+    else:
         say('Good Evening,Boss')
     say('Hope you are having a good day,How can I help you?')
 Greet_Me()
 def Search_Google(query):
+    '''This function searches google for "query" and shows the results.It also speaks out loud a summary of the results'''
     import wikipedia as googleScrap
     query = query.replace('jarvis','')
     query = query.replace('search google for','')
@@ -42,11 +44,12 @@ def Search_Google(query):
     say('Boss ,this is what I found on Google')
     try:
         pywhatkit.search(query)
-        result = googleScrap.summary(query,5)
+        result = googleScrap.summary(query,3)
         say(result)
     except :
         say('Sorry Boss there is no speakable output available')
 def Search_Youtube(query):
+    '''This function searches YouTube for "query" and plays the most popular video/short that it finds.'''
     say('Boss this is what I found for your search on Youtube!')
     query = query.replace('jarvis','')
     query = query.replace('search youtube for','')
@@ -54,51 +57,56 @@ def Search_Youtube(query):
     try:
         web = 'https://www.youtube.com/results?search_query=' + query
         webbrowser.open(web)
-        say('Boss these are the related videos I found Give me a minute I am going to play the most popular one')
+        say('Boss these are the related videos I found ,Give me a minute I am going to play the most popular one')
         pywhatkit.playonyt(query)
         say('Done Boss')
     except:
-        say('Sorry Boss Some error occured I was not able to complete your request')
+        say('Sorry Boss .Some error occurred I was not able to complete your request')
 def Search_Wikipedia(query):
+    '''This function searches wikipedia for "query" and speaks out loud the summary that it finds'''
     say('Searching from Wikipedia......')
     query = query.replace('jarvis','')
     query = query.replace('search wikipedia for', '')
     query = query.replace('wikipedia', '')
-    results = wikipedia.summary(query,sentences = 5)
-    say('According to Wikipedia ......')
-    say(results)
-    print(results)
+    try:
+       results = wikipedia.summary(query,sentences = 3)
+       say('According to Wikipedia ......')
+       say(results)
+       print(results)
+    except:
+       say(f'I am sorry,Boss Either I heard you wrong or there was no information for {query} on wikipedia')
 while True:
     print('Listening')
     say('Listening')
     query = take_command().lower()
     say('Command accepted')
-    sites = [['youtube','https://www.youtube.com'],['wikipedia','https://www.wikipedia.com'],['google','https://www.google.com'],['quora','https://www.quora.com'],['facebook','https://www.facebook.com'],['Github','https://www.github.com'],['Reddit','https://www.reddit.com'],['Discord','https://www.discord.com'],['Instagram','https://www.instagram.com'],['Netflix','https://www.Netflix.com'],['Spotify','https://www.spotify.com']]
-    for site in sites:
-        if  f'Open {site[0]}'.lower() in query:
-            say(f'Opening {site[0]}')
-            webbrowser.open(site[1])
     if 'stop'.lower() in query or 'shutdown'.lower() in query or 'shut down'.lower() in query  or 'shut '.lower() in query:
         say('Okay Boss,Jarvis is turning off')
         quit()
-    if 'the time'.lower() in query:
+    elif 'the time'.lower() in query:
         Time = datetime.datetime.now().strftime('%H:%M:%S')
         say(f'Boss,the time is {Time}')
-    if 'introduce yourself'.lower() in query:
-        say('Boss , I am a virtual voice assistant developed by Mr Shivam Jha and Mr Ishan Tiwari from 10 december 2023 uptill 2024 ,I can help you with almost anything,I can help you with your school work,I can even quiz you,I am capable of face and object recognition,I can tell you the news,predict the weather and do almost everthing models like Siri or Chat Gpt can do')
-    if 'Hello'.lower() in query or 'how are you'.lower() in query:
+    elif 'introduce yourself'.lower() in query:
+        say('I am a virtual voice assistant developed by Mr Shivam Jha and Mr Ishan Tiwari from 10 december 2023 uptill 2024 ,I can help you with almost anything,I can help you with your school work,I can even quiz you,I am capable of face and object recognition,I can tell you the news,predict the weather and do almost everthing models like Siri or Chat Gpt can do')
+    elif 'Hello'.lower() in query or 'how are you'.lower() in query:
         say('Hi Boss,I am fine,how are you doing?')
-    if 'I am fine'.lower() in query:
+    elif 'I am fine'.lower() in query:
         say('That is great Boss')
-    if 'Thank you'.lower() in query:
+    elif 'Thank you'.lower() in query:
         say('You are welcome ,Boss')
-    if 'Search Google'.lower() in query:
+    elif 'open' in query:
+        from Dictapp import openappweb
+        openappweb(query)
+    elif 'close' in query:
+        from Dictapp import closeappweb
+        closeappweb(query)
+    elif 'Search Google'.lower() in query:
         Search_Google(query)
-    if 'Search Youtube'.lower() in query:
+    elif 'Search Youtube'.lower() in query:
         Search_Youtube(query)
-    if 'Search Wikipedia'.lower() in query:
+    elif 'Search Wikipedia'.lower() in query:
         Search_Wikipedia(query)
-    if 'temperature'.lower() in query:
+    elif 'temperature'.lower() in query:
         search = 'temperature in Kolkata'
         url = f'https://search.google.com/search?q={search}'
         r = requests.get(url)
