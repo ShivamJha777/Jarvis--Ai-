d92cc4bcf528f9ebe1d3304730b1da9a0bcf2062
Shivam Jha <sjha42706@gmail.com>
2023-12-15 09:13:12 +0530

    I put the whole thing in a function called Jarvis

--- Dictapp.py
+++ Dictapp.py
@@ -3,24 +3,29 @@ import pyautogui
 import webbrowser
 import win32com.client
 from time import sleep
+did_Find_App = False
 def say(script):
     '''This function says out loud whatever string is given to it'''
     speaker = win32com.client.Dispatch('SAPI.SpVoice')
     return speaker.Speak(script)
 dictapp = {'commandprompt':'cmd','paint':'paint','word':'winword',"excel":'excel',"chrome":'chrome','vscode':'code','powerpoint':'powerpnt','brave':'brave','pycharm':'pycharm','Whatsapp':'Whatsapp','webstorm':'webstorm'}
 def openappweb(query):
-    say('Launching ,Sir')
     if '.com' in query or 'co.in'in query or '.org' in query:
         query = query.replace('open','')
         query = query.replace('jarvis','')
         query = query.replace('launch','')
         query = query.replace(' ','')
+        say(f'Launching {query},Boss')
         webbrowser.open(f'https://www.{query}')
     else:
         keys = list(dictapp.keys())
         for app in keys:
             if app in query:
+                did_Find_App = True
+                say(f'Launching {app} Boss')
                 os.system(f'start {dictapp[app]}')
+        if did_Find_App == False:
+            say(f'Sorry Boss I could not open {app}')
 def closeappweb(query):
     say('Closing,Sir')
     if 'one tab' in query or '1 tab' in query:
