8e58b6fc93651059a95eff2710ad12264410c050
Shivam Jha <sjha42706@gmail.com>
2023-12-23 14:24:03 +0530

    I made a weather function and added some features to the tired function

---
+++ Weather_and_Temperature.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import win32com.client
+def say(Script):
+    '''This function says out loud whatever string is given to it'''
+    speaker = win32com.client.Dispatch('SAPI.SpVoice')
+    return speaker.Speak(Script)
+def find_weather(city):
+
+    # creating url and requests instance
+    url = "https://www.google.com/search?q=" + "weather" + city
+    html = requests.get(url).content
+
+    # getting raw data
+    soup = BeautifulSoup(html, 'html.parser')
+    temp = getattr(soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}),"text",None)
+    str = getattr(soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}),"text",None)
+    data = str.split('\n')
+    time = data[0]
+    sky = data[1]
+    if 'y' not in sky:
+        sky = sky+'y'
+    # printing all data
+    print("Temperature is", temp)
+    print("Time: ", time)
+    print("Sky Description: ", sky)
+    say(f"Temperature is {temp}")
+    say(f"Time forecasted is {time} ")
+    say(f"Sky Description is {sky}")
+def Temperature_finder(Query):
+    url = 'https://www.google.com/search?q=' + Query
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, 'html.parser')
+    temp = getattr(soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}), 'text', None)
+    print(f'Boss the {Query} is {temp}')
+    say(f'Boss the {Query} is {temp}')
+find_weather('mumbai')
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pywhatkit
 import webbrowser
-import requests
-from bs4 import BeautifulSoup
 import win32com.client
 import speech_recognition as sr
 import wikipedia
@@ -9,6 +7,7 @@ import datetime
 import pyautogui
 from keyboard import volumeup , volumedown
 import random
+from time import sleep
 def say(script):
     '''This function says out loud whatever string is given to it'''
     speaker = win32com.client.Dispatch('SAPI.SpVoice')
@@ -77,125 +76,157 @@ def Search_Wikipedia(query):
         print(results)
     except:
         say(f'I am sorry,Boss Either I heard you wrong or there was no information for {query} on wikipedia')
-Greet_Me()
 information = False
 remember = ''
 while True:
     say('Listening')
     print('Listening')
     query = take_command().lower()
-    if 'the time'.lower() in query:
-        Time = datetime.datetime.now().strftime('%H:%M:%S')
-        say(f'Boss,the time is {Time}')
-    elif 'introduce yourself'.lower() in query:
-        say('I am a virtual voice assistant developed by Mr Shivam Jha and Mr Ishan Tiwari from 10 december 2023 uptill 2024,My Name stands for Just A RATHER VERY INTELLIGENT SYSTEM,I can help you with almost anything,I can help you with your school work,I can even quiz you,I am capable of face and object recognition,I can tell you the news,predict the weather and do almost everything models like Siri or Chat Gpt can do')
-    elif 'go offline' in query or 'shut off' in query or 'shutdown' in query  or 'shut down' in query:
-        say('Okay Boss.Jarvis is going offline')
-        quit()
-    elif 'Pause'.lower() in query:
-        pyautogui.press('k')
-        say('Done,Boss.The video has been Paused')
-    elif 'Play'.lower() in query:
-        pyautogui.press('k')
-        say('Done,Boss.The video has been Played')
-    elif 'mute'.lower() in query:
-        pyautogui.press('m')
-        say('Done Boss,The video has been muted')
-    elif 'unmute'.lower() in query:
-        pyautogui.press('m')
-        say('Done Boss,The video has been unmuted')
-    elif 'increase the volume' in query:
-        say('Boss how much percentage would you like the volume to be increase by?Please tell a even number.Only say the percentage value')
-        try:
-            increase_percentage = take_command()
-            increase_percentage = increase_percentage.replace('percent','')
-            increase_percentage = increase_percentage.replace('percentage', '')
-            increase_percentage = increase_percentage.replace('%','')
-            increase_percentage = int(increase_percentage)
-            if increase_percentage % 2 != 0:
-                while increase_percentage % 2 != 0:
-                    increase_percentage += 0.1
-            say('Okay Boss,Turning up the volume')
-            volumeup(increase_percentage)
-        except:
-            say('Sorry Boss ,Some error occurred Please try again')
-    elif 'decrease the volume' in query:
-        say('Boss how much percentage would you like the volume to be increase by?Please tell a even number.')
-        try:
-            decrease_percentage = take_command()
-            decrease_percentage = decrease_percentage.replace('percent', '')
-            decrease_percentage = decrease_percentage.replace('percentage', '')
-            decrease_percentage = decrease_percentage.replace('%', '')
-            decrease_percentage = int(decrease_percentage)
-            if decrease_percentage % 2 != 0:
-                while decrease_percentage % 2 != 0:
-                    decrease_percentage += 1
-            say('Okay Boss Turning down the volume')
-            volumedown(decrease_percentage)
-        except:
-            say('Sorry Boss,Some error occurred please try again')
-    elif 'I am fine'.lower() in query:
-        say('That is great Boss')
-    elif 'tired' or 'cheer' in query:
-        say('Okay Boss Playing some songs to cheer you up')
-        b = random.randint(1,10)
-        if b == 1 :
-            webbrowser.open('https://www.youtube.com/watch?v=kagoEGKHZvU')
-        elif b == 2:
-            webbrowser.open('https://www.youtube.com/watch?v=623JGFAYZ3w')
-        elif b == 3:
-            webbrowser.open('https://www.youtube.com/watch?v=dawrQnvwMTY')
-        elif b == 4:
-            webbrowser.open('https://www.youtube.com/watch?v=2QdPxdcMhFQ')
-        elif b == 5:
-            webbrowser.open('https://www.youtube.com/watch?v=rgGDTO8g2Pg&t=4973s')
-        elif b == 6:
-            webbrowser.open('https://www.youtube.com/watch?v=7aMOurgDB-o')
-        elif b == 7:
-            webbrowser.open('https://www.youtube.com/watch?v=fWRPihlt2ho')
-        elif b == 8:
-            webbrowser.open('https://www.youtube.com/watch?v=MENcEyD0B7w')
-        elif b == 9:
-            webbrowser.open('https://www.youtube.com/watch?v=ajIqCULzFJA')
-        elif b == 10:
-            webbrowser.open('https://www.youtube.com/watch?v=gjkNwuPcl50')
-    elif 'Thank you'.lower() in query:
-        say('You are welcome ,Boss')
-    elif 'open' in query:
-        increase_percentage = take_command()
-        from Dictapp import openappweb
-        openappweb(query)
-    elif 'close' in query:
-        from Dictapp import closeappweb
-        closeappweb(query)
-    elif 'Google'.lower() in query:
-        Search_Google(query)
-    elif 'Youtube'.lower() in query:
-        Search_Youtube(query)
-    elif 'Wikipedia'.lower() in query:
-        Search_Wikipedia(query)
-    elif 'temperature'.lower() in query:
-        search = 'temperature in Kolkata'
-        if 'in' in query:
-            query = query.replace('what is the','')
-            query = query.replace('jarvis','')
-            search = query
-        url = f'https://search.google.com/search?q={search}'
-        r = requests.get(url)
-        data = BeautifulSoup(r.text,'html.parser')
-        temp = getattr(data.find('div', class_ = "BNeawe"), 'text', None)
-        say(f'Boss the current {search} is {temp}')
-    elif 'remember that' in query:
-        query = query.replace('jarvis','')
-        query = query.replace('i want you to', '')
-        query = query.replace('remember that', '')
-        query = query.replace('i', 'you')
-        remember += query
-        say(f'OKay Boss,I wil remember {remember}')
-    elif 'What do you remember' in query:
-        say(f'Boss,You told me to remember that {remember}')
-    elif 'forget' in query:
-        say('Okay Boss I am Going to forget whatever you told me')
-        remember = ''
-    elif 'Hello'.lower() or 'how are you'.lower() in query:
-        say('Sorry Boss,I could not understand what you just said')
+    if 'wake up' in query:
+        Greet_Me()
+        while True:
+            say('Listening')
+            print('Listening')
+            query = take_command()
+            if 'the time'.lower() in query:
+                Time = datetime.datetime.now().strftime('%H:%M:%S')
+                say(f'Boss,the time is {Time}')
+            elif 'introduce yourself'.lower() in query:
+                say('I am a virtual voice assistant developed by Mr Shivam Jha and Mr Ishan Tiwari from 10 december 2023 uptill 2024,My Name stands for Just A RATHER VERY INTELLIGENT SYSTEM,I can help you with almost anything,I can help you with your school work,I can even quiz you,I am capable of face and object recognition,I can tell you the news,predict the weather and do almost everything models like Siri or Chat Gpt can do')
+            elif 'go offline' in query or 'shut off' in query or 'shutdown' in query  or 'shut down' in query:
+                say('Okay Boss.Jarvis is going offline')
+                quit()
+            elif 'Pause'.lower() in query:
+                pyautogui.press('k')
+                say('Done,Boss.The video has been Paused')
+            elif 'Play'.lower() in query:
+                pyautogui.press('k')
+                say('Done,Boss.The video has been Played')
+            elif 'mute'.lower() in query:
+                pyautogui.press('m')
+                say('Done Boss,The video has been muted')
+            elif 'unmute'.lower() in query:
+                pyautogui.press('m')
+                say('Done Boss,The video has been unmuted')
+            elif 'increase the volume' in query:
+                say('Boss how much percentage would you like the volume to be increase by?Please tell a even number.Only say the percentage value')
+                try:
+                    increase_percentage = take_command()
+                    increase_percentage = increase_percentage.replace('percent','')
+                    increase_percentage = increase_percentage.replace('percentage', '')
+                    increase_percentage = increase_percentage.replace('%','')
+                    increase_percentage = int(increase_percentage)
+                    if increase_percentage % 2 != 0:
+                        while increase_percentage % 2 != 0:
+                            increase_percentage += 0.1
+                    say('Okay Boss,Turning up the volume')
+                    volumeup(increase_percentage)
+                except:
+                    say('Sorry Boss ,Some error occurred Please try again')
+            elif 'decrease the volume' in query:
+                say('Boss how much percentage would you like the volume to be increase by?Please tell a even number.')
+                try:
+                    decrease_percentage = take_command()
+                    decrease_percentage = decrease_percentage.replace('percent', '')
+                    decrease_percentage = decrease_percentage.replace('percentage', '')
+                    decrease_percentage = decrease_percentage.replace('%', '')
+                    decrease_percentage = int(decrease_percentage)
+                    if decrease_percentage % 2 != 0:
+                        while decrease_percentage % 2 != 0:
+                            decrease_percentage += 1
+                    say('Okay Boss Turning down the volume')
+                    volumedown(decrease_percentage)
+                except:
+                    say('Sorry Boss,Some error occurred please try again')
+            elif 'I am fine'.lower() in query:
+                say('That is great Boss')
+            elif 'tired' in  query:
+                say('Okay Boss Playing some songs to cheer you up')
+                play_songs = True
+                while play_songs:
+                    b = random.randint(1,10)
+                    if b == 1 :
+                        webbrowser.open('https://www.youtube.com/watch?v=kagoEGKHZvU')
+                        sleep(210)
+                    elif b == 2:
+                        webbrowser.open('https://www.youtube.com/watch?v=623JGFAYZ3w')
+                        sleep(329)
+                    elif b == 3:
+                        webbrowser.open('https://www.youtube.com/watch?v=dawrQnvwMTY')
+                        sleep(195)
+                    elif b == 4:
+                        webbrowser.open('https://www.youtube.com/watch?v=2QdPxdcMhFQ')
+                        sleep(236)
+                    elif b == 5:
+                        webbrowser.open('https://www.youtube.com/watch?v=YkJvHe3KK2c')
+                        sleep(123)
+                    elif b == 6:
+                        webbrowser.open('https://www.youtube.com/watch?v=7aMOurgDB-o')
+                        sleep(108)
+                    elif b == 7:
+                        webbrowser.open('https://www.youtube.com/watch?v=fWRPihlt2ho')
+                        sleep(123)
+                    elif b == 8:
+                        webbrowser.open('https://www.youtube.com/watch?v=MENcEyD0B7w')
+                        sleep(177)
+                    elif b == 9:
+                        webbrowser.open('https://www.youtube.com/watch?v=ajIqCULzFJA')
+                        sleep(116.9)
+                    elif b == 10:
+                        webbrowser.open('https://www.youtube.com/watch?v=gjkNwuPcl50')
+                        sleep(122.5)
+                    say('Boss Would you like me to keep playing songs?Please reply with a yes or no')
+                    choice = take_command()
+                    if 'no' in choice:
+                        play_songs = False
+            elif 'Thank you'.lower() in query:
+                say('You are welcome ,Boss')
+            elif 'open' in query:
+                increase_percentage = take_command()
+                from Dictapp import openappweb
+                openappweb(query)
+            elif 'close' in query:
+                from Dictapp import closeappweb
+                closeappweb(query)
+            elif 'Google'.lower() in query:
+                Search_Google(query)
+            elif 'Youtube'.lower() in query:
+                Search_Youtube(query)
+            elif 'Wikipedia'.lower() in query:
+                Search_Wikipedia(query)
+            elif 'sleep' in query:
+                say('Okay Boss,Going to sleep')
+                break
+            elif 'temperature'.lower() in query:
+                search = 'temperature in Kolkata'
+                if 'in' in query:
+                    query = query.replace('what is the','')
+                    query = query.replace('jarvis','')
+                    search = query
+                from Weather_and_Temperature import Temperature_finder
+                Temperature_finder(search)
+            elif 'weather' in query:
+                from Weather_and_Temperature import find_weather
+                query = query.replace('jarvis','')
+                query = query.replace('how is the weather in', '')
+                query = query.replace('what is the weather in', '')
+                query = query.replace('tell me ', '')
+                query = query.replace('how the weather in', '')
+                query = query.replace('is', '')
+                The_city = query
+
+                find_weather(The_city)
+            elif 'remember that' in query:
+                query = query.replace('jarvis','')
+                query = query.replace('i want you to', '')
+                query = query.replace('remember that', '')
+                query = query.replace('i', 'you')
+                remember += query
+                say(f'OKay Boss,I wil remember {remember}')
+            elif 'What do you remember' in query:
+                say(f'Boss,You told me to remember that {remember}')
+            elif 'forget' in query:
+                say('Okay Boss I am Going to forget whatever you told me')
+                remember = ''
+            elif 'Hello'.lower() or 'how are you'.lower() in query:
+                say('Sorry Boss,I could not understand what you just said')
