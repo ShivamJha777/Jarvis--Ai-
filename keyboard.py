2ad6d202e64e34455247cdb7ccd14659063740ba
Shivam Jha <146080234+ShivamJha777@users.noreply.github.com>
2023-12-28 00:34:00 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -17,9 +17,8 @@ def volumeup(percentage):
     except:
         say('Sorry Boss,Some error occured ,Please try again')
 def volumedown(percentage):
-    number_of_times = percentage/2
+    number_of_times = percentage//2
     try:
-        number_of_times = int(number_of_times)
         for i in range(number_of_times):
             keyboard.press(Key.media_volume_down)
             keyboard.release(Key.media_volume_down)
